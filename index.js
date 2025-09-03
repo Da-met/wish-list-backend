d721e1f2ad453a452e1fba6d5cc7b1f2bd41f7f6
Da-met <dmetelskaya@yandex.ru>
2025-09-03 18:17:28 +0300

    Fix 01

--- index.js
+++ index.js
@@ -20,18 +20,17 @@ const app = express();
 const allowedOrigins = [
     'http://localhost:5173',   // фронт локально
     'https://vishy.vercel.app' // фронт на Vercel
-  ];
+];
   
-  app.use(cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true
-  }));
+app.use(cors({
+origin: allowedOrigins,
+methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+allowedHeaders: ['Content-Type', 'Authorization'],
+credentials: true
+}));
+
+// Чтобы Express не ругался на OPTIONS
+app.options('*', cors());
 
 app.use(cookieParser()); 
 

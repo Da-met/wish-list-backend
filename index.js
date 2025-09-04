3978ecceb8e89cb86e3d40c6460751a69469f652
Da-met <dmetelskaya@yandex.ru>
2025-09-04 11:56:52 +0300

    fix: vercel deployment config 0.2

--- index.js
+++ index.js
@@ -23,11 +23,15 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-    origin: allowedOrigins,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
-    methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
-    allowedHeaders: ['Content-Type','Authorization','Accept'],
-}));
+  }));
 
 // Обработка preflight-запросов
 app.options('*', cors());

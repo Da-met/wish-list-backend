6f0a13b3d3db45db96b130a206e8dbd6fbbcf441
Da-met <dmetelskaya@yandex.ru>
2025-09-08 12:07:06 +0300

    fix: update pg config

--- db.js
+++ db.js
@@ -1,22 +1,45 @@
-const {Sequelize} = require('sequelize')
+const { Sequelize } = require('sequelize');
+const pg = require('pg'); // 👈 явное подключение pg
 
-module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false, // важно для Supabase
-            },
-        },
-        logging: false, // отключаем лишние логи
-        // dialectModuleOptions: {
-        //     family: 6 // используем IPv6
-        // }
-    }
-)
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    dialectModule: pg, // 👈 заставляем использовать pg
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // важно для Supabase на Vercel
+      },
+    },
+    logging: false, // отключаем лишние логи
+  }
+);
+
+module.exports = sequelize;
+
+
+
+// const {Sequelize} = require('sequelize')
+
+// module.exports = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         dialect: 'postgres',
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//         dialectOptions: {
+//             ssl: {
+//                 require: true,
+//                 rejectUnauthorized: false, 
+//             },
+//         },
+//         logging: false, 
+//     }
+// )

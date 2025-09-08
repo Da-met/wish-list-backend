a3d280ed9afb1d6273dda074ed8f41aeed60695d
Da-met <dmetelskaya@yandex.ru>
2025-09-08 17:20:46 +0300

    switch to DATABASE_URL

--- db.js
+++ db.js
@@ -1,26 +1,20 @@
 const { Sequelize } = require('sequelize');
-const pg = require('pg'); // 👈 явное подключение pg
+const pg = require('pg'); // 👈 важно для Vercel
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
-    dialect: 'postgres',
-    dialectModule: pg, // 👈 заставляем использовать pg
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // важно для Supabase на Vercel
-      },
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectModule: pg,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // нужно для Supabase
     },
-    logging: false, // отключаем лишние логи
-  }
-);
+  },
+  logging: false,
+});
 
 module.exports = sequelize;
+
 
 
 
